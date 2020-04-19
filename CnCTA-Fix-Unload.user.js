cb3bb7074cc29b87010779d8607f7534dfd87522
Bloofi <bloofi@gmail.com>
2020-04-19 22:05:15 +0200

    feat : add Game Unload fix

---
+++ CnCTA-Fix-Unload.user.js
@@ -0,0 +1,37 @@
+"use strict";
+// ==UserScript==
+// @version	    2020.04.19
+// @name        CnCTA Fix Unload
+// @downloadURL https://github.com/bloofi/CnC_TA/raw/master/CnCTA-Fix-Unload.user.js
+// @updateURL   https://github.com/bloofi/CnC_TA/raw/master/CnCTA-Fix-Unload.user.js
+// @include     http*://prodgame*.alliances.commandandconquer.com/*/index.aspx*
+// @include     http*://cncapp*.alliances.commandandconquer.com/*/index.aspx*
+// @author      ShockrNZ (https://github.com/blacha)
+//              bloofi (https://github.com/bloofi)
+// ==/UserScript==
+(function () {
+    const script = () => {
+        const fixedWindow = window;
+        fixedWindow._addEventListener = window.addEventListener;
+        window.addEventListener = function (a, b, c) {
+            if (a == 'unload') {
+                return;
+            }
+            return fixedWindow._addEventListener(a, b, c);
+        };
+    };
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Script injection
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    if (/commandandconquer\.com/i.test(document.domain)) {
+        try {
+            const script_block = document.createElement('script');
+            script_block.innerHTML = `(${script.toString()})();`;
+            script_block.type = 'text/javascript';
+            document.getElementsByTagName('head')[0].appendChild(script_block);
+        }
+        catch (e) {
+            console.log('Failed to inject script', e);
+        }
+    }
+})();
